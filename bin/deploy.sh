412087665dc57924a4b15b8755d54d0eab37a56c
Vladislav Osmianskij <vlaosm@gmail.com>
2021-08-30 12:48:41 +0300

    minor: app/data deployment script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,17 +1,36 @@
 #!/bin/bash
 
-git pull
-php vendor/osmphp/framework/bin/console.php index
+# `osm`, `osmt` and other Bash aliases don't work in Bash files.
+# The following variables are used instead of these Bash aliases
+OSM="php vendor/osmphp/framework/bin/console.php"
+OSMC="php vendor/osmphp/core/bin/compile.php"
+OSMT="php vendor/osmphp/framework/bin/tools.php"
 
-# full update
+# Current Git branch
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
-#osm http:down
-#git pull
-#composer install
-#osmc Osm_Tools
-#osmt config:npm
-#npm install
-#gulp
-#osm migrate:up
-#osm index
-#osm http:down
+#git fetch
+
+# Added, modified and deleted files overall and outside the `/data` directory
+ALL_CHANGES=$(git diff --name-only $BRANCH..origin/$BRANCH)
+CODE_CHANGES=$(git diff --name-only $BRANCH..origin/$BRANCH :^data/)
+
+# If there are no code changes, update data files and the index, otherwise,
+# do full update
+if [[ $ALL_CHANGES ]] && ! [[ $CODE_CHANGES ]]; then
+    git merge origin/$BRANCH
+    $OSM index
+    echo "DATA UPDATED!"
+else
+    $OSM http:down
+    git merge origin/$BRANCH
+    composer install
+    $OSMC Osm_Tools
+    $OSMT config:npm
+    npm install
+    gulp
+    $OSM migrate:up
+    $OSM index
+    $OSM http:up
+    echo "APP UPDATED!"
+fi
