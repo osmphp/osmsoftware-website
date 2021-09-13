19745e8802e3e7dafa397cf3484741df4dd98834
Vladislav Osmianskij <vlaosm@gmail.com>
2021-09-13 18:12:19 +0300

    minor: custom fonts added to `my` theme

--- themes/_front__my/tailwind.config.js
+++ themes/_front__my/tailwind.config.js
@@ -1,3 +1,5 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
     purge: {
         content: [
@@ -6,7 +8,11 @@ module.exports = {
     },
     darkMode: false, // or 'media' or 'class'
     theme: {
-        extend: {}
+        fontFamily: {
+          'sans': ['"Titillium Web"', ...defaultTheme.fontFamily.sans],
+          'serif': [...defaultTheme.fontFamily.serif],
+          'mono': ['"Syne Mono"', ...defaultTheme.fontFamily.mono]
+        }
     },
     variants: {},
     plugins: [
