77adabadc1a14770b4c53f0f071c3c6006848a08
Vladislav Osmianskij <vlaosm@gmail.com>
2021-09-15 22:26:12 +0300

    fix: error page titles

--- themes/_front__my/views/std-pages/503.blade.php
+++ themes/_front__my/views/std-pages/503.blade.php
@@ -2,7 +2,7 @@
 global $osm_app; /* @var \Osm\Core\App $osm_app */
 $theme_url = "{$osm_app->http->base_url}/{$osm_app->theme->name}";
 ?>
-<x-std-pages::layout title='Page not found | Osm Software'>
+<x-std-pages::layout title='On Maintenance | Osm Software'>
     <div class="container mx-auto px-4">
         <h1 class="text-2xl sm:text-4xl pt-6 mb-8 text-center border-t border-gray-300">
             {{ \Osm\__("The website is being updated")}}
