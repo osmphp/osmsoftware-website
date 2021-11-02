acc4329bdf18b5dbdfb76f8b01cb2109506d2510
Vladislav Osmianskij <vlaosm@gmail.com>
2021-11-02 14:07:38 +0200

    minor: home page changes

--- themes/_front__my/views/pages/home.blade.php
+++ themes/_front__my/views/pages/home.blade.php
@@ -12,7 +12,7 @@ $categories = $osm_app->modules[\Osm\Blog\Categories\Module::class]->categories;
 >
     <div class="container mx-auto px-4 grid grid-cols-12 gap-4">
         <?php $category = $categories['framework']; ?>
-        <section class="col-span-12 lg:col-start-4 lg:col-span-6 mb-6">
+        <section class="col-span-12 lg:col-start-1 lg:col-span-6 mb-6">
             <h1 class="text-2xl sm:text-4xl text-center mt-16
                 text-{{ $category->color }}"
             >
@@ -43,9 +43,32 @@ $categories = $osm_app->modules[\Osm\Blog\Categories\Module::class]->categories;
             </p>
         </section>
 
+        <?php $category = $categories['data']; ?>
+        <section class="col-span-12 lg:col-start-7 lg:col-span-6 mb-6">
+            <h2 class="text-2xl sm:text-4xl text-center mt-16
+                text-{{ $category->color }}">
+                Osm Admin
+            </h2>
+            <p class="text-lg mt-8">
+                Previously known under *The Big Red Button* codename, it's a package
+                for defining application data structures and logic using PHP 8 classes and attributes,
+                pressing *the big red button*, and getting fully functioning Admin Panel and API.
+            </p>
+            <p class="mt-8 text-center flex flex-wrap justify-center gap-2 text-white">
+                <a class="py-2 px-4 rounded bg-{{ $category->color }}
+                    hover:bg-{{ $category->hover_color }}"
+                    href="{{ "{$osm_app->http->base_url}/blog/data/" }}" title="Blog"
+                >Blog</a>
+                <a class="py-2 px-4 rounded bg-{{ $category->color }}
+                    hover:bg-{{ $category->hover_color }}"
+                    href="https://github.com/osmphp/data" title="Source"
+                >Source</a>
+            </p>
+        </section>
+
         @if ($news)
             <?php $category = $categories['news']; ?>
-            <section class="col-span-12 lg:col-start-4 lg:col-span-6 mb-6">
+            <section class="col-span-12 lg:col-start-1 lg:col-span-6 mb-6">
                 <h2 class="text-2xl sm:text-4xl text-center mt-16
                     text-{{ $category->color }}">
                     @if ($news->main_category_file)
@@ -69,7 +92,7 @@ $categories = $osm_app->modules[\Osm\Blog\Categories\Module::class]->categories;
         @endif
 
         <?php $category = $categories['osmsoftware']; ?>
-        <section class="col-span-12 lg:col-start-1 lg:col-span-6 mb-6">
+        <section class="col-span-12 lg:col-start-7 lg:col-span-6 mb-6">
             <h2 class="text-2xl sm:text-4xl text-center mt-16
                 text-{{ $category->color }}">
                 Reference Project: osm.software
@@ -94,28 +117,6 @@ $categories = $osm_app->modules[\Osm\Blog\Categories\Module::class]->categories;
                 <a class="py-2 px-4 rounded bg-{{ $category->color }}
                     hover:bg-{{ $category->hover_color }}"
                     href="https://github.com/osmphp/osmsoftware-website" title="Source"
-                >Source</a>
-            </p>
-        </section>
-
-
-        <?php $category = $categories['data']; ?>
-        <section class="col-span-12 lg:col-start-7 lg:col-span-6 mb-6">
-            <h2 class="text-2xl sm:text-4xl text-center mt-16
-                text-{{ $category->color }}">
-                Current Project: The Big Red Button
-            </h2>
-            <p class="text-lg mt-8">
-                It's a project about defining application data structures and logic in PHP code, pressing *the big red button* (hence the codename), and getting fully functioning Admin Panel and API.
-            </p>
-            <p class="mt-8 text-center flex flex-wrap justify-center gap-2 text-white">
-                <a class="py-2 px-4 rounded bg-{{ $category->color }}
-                    hover:bg-{{ $category->hover_color }}"
-                    href="{{ "{$osm_app->http->base_url}/blog/data/" }}" title="Blog"
-                >Blog</a>
-                <a class="py-2 px-4 rounded bg-{{ $category->color }}
-                    hover:bg-{{ $category->hover_color }}"
-                    href="https://github.com/osmphp/data" title="Source"
                 >Source</a>
             </p>
         </section>
